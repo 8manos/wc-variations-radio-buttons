7207d7fcc6c1771094341bbfcd0bfdab7913f17f
mantish <mauricio@8manos.com>
2015-08-13 21:46:21 -0500

    some actions from wc 2.4

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -29,6 +29,8 @@ global $product, $post;
 <?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
 <form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo $post->ID; ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
+	<?php do_action( 'woocommerce_before_variations_form' ); ?>
+
 	<?php if ( ! empty( $available_variations ) ) : ?>
 		<table class="variations" cellspacing="0">
 			<tbody>
@@ -111,6 +113,7 @@ global $product, $post;
 
 	<?php endif; ?>
 
+	<?php do_action( 'woocommerce_after_variations_form' ); ?>
 </form>
 
 <?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
