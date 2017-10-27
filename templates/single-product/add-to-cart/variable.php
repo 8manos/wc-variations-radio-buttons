3ade832db04af087c0cb37472f0132c44fb37028
Nico Mollet <nico.mollet@gmail.com>
2017-10-27 10:02:56 +0200

    Adding classes for inline radio buttons
    I added new classes, to be able to style each attribute, and make some radio buttons inline.
    
    Thank you.

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -24,7 +24,7 @@ if ( ! function_exists( 'print_attribute_radio' ) ) {
 		$esc_value = esc_attr( $value );
 		$id = esc_attr( $name . '_v_' . $value . $product->get_id() ); //added product ID at the end of the name to target single products
 		$filtered_label = apply_filters( 'woocommerce_variation_option_name', $label );
-		printf( '<div><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s><label for="%3$s">%5$s</label></div>', $input_name, $esc_value, $id, $checked, $filtered_label );
+		printf( '<div class="attribute-radio"><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s><label for="%3$s">%5$s</label></div>', $input_name, $esc_value, $id, $checked, $filtered_label );
 	}
 }
 
@@ -43,7 +43,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 		<table class="variations" cellspacing="0">
 			<tbody>
 				<?php foreach ( $attributes as $name => $options ) : ?>
-					<tr>
+					<tr class="attribute-<?php echo sanitize_title($name); ?>">
 						<td class="label"><label for="<?php echo sanitize_title( $name ); ?>"><?php echo wc_attribute_label( $name ); ?></label></td>
 						<?php
 						$sanitized_name = sanitize_title( $name );
