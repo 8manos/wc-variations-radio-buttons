bf9cd2bcf5e7934ff24b8e836f0dd560d91cd637
mantish <mauriciomantilla@gmail.com>
2017-11-22 00:18:26 +0100

    move print_attribute_radio function out of the template

--- wc-variations-radio-buttons.php
+++ wc-variations-radio-buttons.php
@@ -89,4 +89,17 @@ if ( is_plugin_active( 'woocommerce/woocommerce.php') ) {
 	}
 
 	new WC_Radio_Buttons();
+
+	if ( ! function_exists( 'print_attribute_radio' ) ) {
+		function print_attribute_radio( $checked_value, $value, $label, $name ) {
+			global $product;
+
+			$input_name = 'attribute_' . esc_attr( $name ) ;
+			$esc_value = esc_attr( $value );
+			$id = esc_attr( $name . '_v_' . $value . $product->get_id() ); //added product ID at the end of the name to target single products
+			$checked = checked( $checked_value, $value, false );
+			$filtered_label = apply_filters( 'woocommerce_variation_option_name', $label );
+			printf( '<div><input type="radio" name="%1$s" value="%2$s" id="%3$s" %4$s><label for="%3$s">%5$s</label></div>', $input_name, $esc_value, $id, $checked, $filtered_label );
+		}
+	}
 }
