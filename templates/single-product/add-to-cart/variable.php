cd26c3003ebc7adcc1723942cd4d5f8e180f7adb
mantish <mauriciomantilla@gmail.com>
2023-11-01 14:07:48 +0100

    update variable template to match latest from WooCommerce

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -2,9 +2,10 @@
 /**
  * Variable product add to cart
  *
- * @author  WooThemes
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/variable.php.
+ *
  * @package WooCommerce/Templates
- * @version 3.4.1
+ * @version 2.0.6
  *
  * Modified to use radio buttons instead of dropdowns
  * @author 8manos
@@ -16,21 +17,23 @@ global $product;
 global $woocommerce;
 
 $attribute_keys = array_keys( $attributes );
+$variations_json = wp_json_encode($available_variations);
+$variations_attr = function_exists('wc_esc_json') ? wc_esc_json($variations_json) : _wp_specialchars($variations_json, ENT_QUOTES, 'UTF-8', true);
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ) ?>">
+<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+		<p class="stock out-of-stock"><?php echo esc_html( apply_filters( 'woocommerce_out_of_stock_message', __( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ) ); ?></p>
 	<?php else : ?>
-		<table class="variations" cellspacing="0">
+		<table class="variations" cellspacing="0"  role="presentation">
 			<tbody>
 				<?php foreach ( $attributes as $name => $options ) : ?>
 					<?php $sanitized_name = sanitize_title( $name ); ?>
 					<tr class="attribute-<?php echo esc_attr( $sanitized_name ); ?>">
-						<td class="label"><label for="<?php echo esc_attr( $sanitized_name ); ?>"><?php echo wc_attribute_label( $name ); ?></label></td>
+						<th class="label"><label for="<?php echo esc_attr( $sanitized_name ); ?>"><?php echo wc_attribute_label( $name ); // WPCS: XSS ok. ?></label></th>
 						<?php
 						if ( isset( $_REQUEST[ 'attribute_' . $sanitized_name ] ) ) {
 							$checked_value = $_REQUEST[ 'attribute_' . $sanitized_name ];
@@ -67,6 +70,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 				<?php endforeach; ?>
 			</tbody>
 		</table>
+		<?php do_action( 'woocommerce_after_variations_table' ); ?>
 
 		<?php
 			if ( version_compare($woocommerce->version, '3.4.0') < 0 ) {
@@ -92,4 +96,5 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php do_action( 'woocommerce_after_variations_form' ); ?>
 </form>
 
-<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+<?php
+do_action( 'woocommerce_after_add_to_cart_form' ); ?>
